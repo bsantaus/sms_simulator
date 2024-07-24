122719b4e714485a7bfc4e6a76f64723b269e163
Ben Santaus <bsantaus2026@gmail.com>
2024-07-24 19:25:27 -0400

    generator pre-queue all set

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -3,21 +3,46 @@ import string
 
 from dataclasses import dataclass
 
+MAX_STRING_LEN = 100
+MIN_STRING_LEN = 1
+PHONE_LEN = 10
+
 @dataclass
 class Message():
     message: str
     phone: str
 
-def create_random_string():
-    string_len = random.randint(1, 100)
-    return "".join([random.choice(string.printable) for _ in range(string_len)])
+class Generator():
 
-def create_random_phone_number():
-    return "".join([random.choice(string.digits) for _ in range(10)])
+    def __init__(self, num_messages=1000):
+        self.num_messages = num_messages
+    
+    @property
+    def num_messages(self):
+        return self._num_messages
 
-def generate_message():
-    return Message(
-        message=create_random_string(),
-        phone=create_random_phone_number()
-    )
+    @num_messages.setter
+    def num_messages(self, value):
+        if value < 0:
+            raise ValueError("Number of messages must be non-negative!")
+        self._num_messages = value
 
+    def create_random_string(self):
+        string_len = random.randint(MIN_STRING_LEN, MAX_STRING_LEN)
+        return "".join([random.choice(string.printable) for _ in range(string_len)])
+
+    def create_random_phone_number(self):
+        return "".join([random.choice(string.digits) for _ in range(PHONE_LEN)])
+
+    def generate_message(self):
+        return Message(
+            message=self.create_random_string(),
+            phone=self.create_random_phone_number()
+        )
+
+    def start_generating(self):
+        for _ in range(self.num_messages):
+            self.push_message(self.generate_message())
+    
+    def push_message(self, message):
+        pass
--- test/generator/test_generator.py
+++ test/generator/test_generator.py
@@ -1,17 +1,36 @@
 import pytest
-from generator.generator import *
+from unittest.mock import MagicMock
+
+from generator.generator import Generator, Message
+
 
 MAX_STRING_LEN = 100
 MIN_STRING_LEN = 1
 PHONE_LEN = 10
 
-def test_create_random_string():
-    rand_string = create_random_string()
+@pytest.fixture
+def default_gen():
+    return Generator()
+
+def test_constructor(default_gen):
+    assert default_gen.num_messages == 1000
+
+    gen_5 = Generator(5)
+    assert gen_5.num_messages == 5
+
+    gen_0 = Generator(0)
+    assert gen_0.num_messages == 0
+
+    with pytest.raises(ValueError):
+        gen_minus_one = Generator(-1)       
+
+def test_create_random_string(default_gen):
+    rand_string = default_gen.create_random_string()
 
     assert type(rand_string) == str
     assert len(rand_string) >= MIN_STRING_LEN and len(rand_string) <= MAX_STRING_LEN
 
-    rand_string_1 = create_random_string()
+    rand_string_1 = default_gen.create_random_string()
     assert len(rand_string) >= MIN_STRING_LEN and len(rand_string) <= MAX_STRING_LEN
 
     assert rand_string != rand_string_1
@@ -20,25 +39,25 @@ def test_create_random_string():
     # check sequence of characters being generated is not the same even if lengths are different
     assert rand_string[:len(rand_string_1)] != rand_string_1[:len(rand_string)]
 
-def test_create_random_phone_number():
+def test_create_random_phone_number(default_gen):
 
     # will not be checking that phone numbers are _valid_ 
     # but will check that they're a correct length
     # (assuming all numbers are in US)
 
-    rand_phone = create_random_phone_number()
+    rand_phone = default_gen.create_random_phone_number()
 
     assert type(rand_phone) == str
     assert len(rand_phone) == PHONE_LEN
 
-    rand_phone_1 = create_random_phone_number()
+    rand_phone_1 = default_gen.create_random_phone_number()
     assert len(rand_phone_1) == PHONE_LEN
 
     assert rand_phone != rand_phone_1
 
-def test_generate_message():
+def test_generate_message(default_gen):
 
-    message = generate_message()
+    message = default_gen.generate_message()
 
     assert type(message) == Message
 
@@ -47,3 +66,32 @@ def test_generate_message():
 
     assert type(message.phone) == str
     assert len(message.phone) == PHONE_LEN
+
+def test_start_generating(default_gen):
+
+    default_gen.push_message = MagicMock()
+
+    default_gen.start_generating()
+
+    assert default_gen.push_message.call_count == default_gen.num_messages
+    assert type(default_gen.push_message.call_args[0][0]) == Message
+
+    generate_10 = Generator(10)
+
+    generate_10.push_message = MagicMock()
+    generate_10.start_generating()
+
+    assert generate_10.push_message.call_count == 10
+
+    generate_10.push_message.call_count = 0
+    generate_10.num_messages = 100
+
+    generate_10.start_generating()
+    assert generate_10.push_message.call_count == 100
+
+    generate_0 = Generator(0)
+
+    generate_0.push_message = MagicMock()
+
+    generate_0.start_generating()
+    assert generate_0.push_message.call_count == 0
